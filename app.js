201fcafa98ed00b94e4b76155b5da11a0029e184
adecess <axel.saliniere@protonmail.com>
2025-08-12 13:20:11 -0400

    feat: update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-import DataProcessor from "./hexagon/DataProcessor.js";
-import CliAdapter from "./adapters/cli/CliAdapter.js";
+import { FilterUseCase } from "./hexagon/FilterUseCase.js";
+import { CountUseCase } from "./hexagon/CountUseCase.js";
+
+import { CliAdapter } from "./adapters/cli/CliAdapter.js";
+
 import data from "./data.js";
 
 // Set up the hexagon
-const dataProcessor = new DataProcessor();
+const filterUseCase = new FilterUseCase();
+const countUseCase = new CountUseCase();
 
 // Inject hexagon into CLI adapter
-const cliAdapter = new CliAdapter(dataProcessor);
+const cliAdapter = new CliAdapter(filterUseCase, countUseCase);
 
 // Execute
 cliAdapter.execute(process.argv.slice(2), data);
